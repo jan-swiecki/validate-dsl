4c4974712d92b7a2410b50f7db3b008d5f333264
Jan Swiecki <jan.swiecki@gmail.com>
2015-05-10 02:52:55 +0200

    Successful esrefactor based scope rewiring

--- src/validate-dsl/services/rewirescope.js
+++ src/validate-dsl/services/rewirescope.js
@@ -16,23 +16,42 @@ angular.module('validateDsl').service('RewireScope', function(esrefactor, _) {
 	}
 
 	// e.g.
-	//      rewireScope(function() { test(); test2(); }, "Rules", {test: "whatever"})
+	//      rewire(function() { test(); test2(); }, "Rules", {test: "whatever"})
 	//      will return function: function() { Rules.test(); test2(); }
-	RewireScope.rewireScope = function(callback, key, newScope) {
-		var code = callback.toString();
+	RewireScope.rewire = function(callback, prefix, newScope) {
+		// var code = "(function(){ return "+callback.toString()+"; })()";
+		var code = RewireScope.wrapFn(callback.toString());
 		var ctx = new esrefactor.Context(code);
 
 		var keys = _.keys(newScope);
 
-		// _.each
-		
-		var id = ctx.identify(12);
-	}
+		var newCode = code;
+
+		_.each(keys, function(key){
+			var indices = RewireScope.getIndices(newCode, key);
+			for(var k in indices) {
+				if(indices.hasOwnProperty(k)) {
+					var index = indices[k];
+					var id = ctx.identify(index);
+					if(id) {
+						var name = _.result(id, "identifier.name");
+						if(_.isNull(id.declaration) && name) {
+							// newCode = ctx.rename(id, key+"."+name);
+							newCode = ctx.rename(id, prefix+"."+name);
+							ctx = new esrefactor.Context(newCode);
+							break;
+						}
+					}
+				}
+			}
+		});
+
+		return eval(newCode);
+	};
 
 	RewireScope.getIndices = function(text, searchString, indices, start) {
 		indices = indices || [];
 		start = start || 0;
-		console.log("getIndices", text, searchString);
 		if(text.length === 0) {
 			return indices;
 		} else {
@@ -43,12 +62,15 @@ angular.module('validateDsl').service('RewireScope', function(esrefactor, _) {
 			} else {
 				return RewireScope.getIndices("", searchString, indices, start);
 			}
-			// if(index !== -1) {
-			// 	indices.push(index);
-			// }
-			// return RewireScope.getIndices(text.substr(index+1), searchString, indices);
+		}
+	};
+
+	RewireScope.wrapFn = function(fnString) {
+		if(typeof fnString === 'function') {
+			fnString = fnString.toString();
 		}
 
+		return "(function() { return "+fnString+"; })()";
 	}
 
 	function isFromOutsideScope(id) {
--- test/unit/validate-dsl/services/rewirescope.js
+++ test/unit/validate-dsl/services/rewirescope.js
@@ -7,45 +7,99 @@ describe('Service: RewireScope', function () {
 
   // instantiate service
   var RewireScope;
-  beforeEach(inject(function (_RewireScope_) {
+  var esprima;
+  var esrefactor;
+  var _;
+  beforeEach(inject(function (_RewireScope_, _esprima_, ___, _esrefactor_) {
     RewireScope = _RewireScope_;
+    esprima = _esprima_;
+    _ = ___;
+    esrefactor = _esrefactor_;
   }));
 
   it('should do something', function () {
     (!!RewireScope).should.equal(true);
   });
 
-  it('rewire scope', function() {
-
-    var code = "function(){\
-      getNullRule(context.isNullable, isDate.then(True).otherwise(False(\"$0 is not date\")))\
-    }";
-
-    var targetCode = "function(){\
-      Rules.getNullRule(context.isNullable, Rules.isDate.then(Rules.True).otherwise(Rules.False(\"$0 is not date\")))\
-    }";
-    
-    // var ctx = new esrefactor.Context(code);
-    // var id = ctx.identify(12);
-    // ctx.rename(id, "Rules.x").should.equal("console.log(Rules.x); Rules.x()");
-
-    (true).should.equal(true);
-
-
-  });
-
-  it('should getIndices properly', function(){
-
+  it('should getIndices properly 1', function(){
     var text = "ala ma kota i ma psa";
-
     var indices = RewireScope.getIndices(text, "ma");
-
-    // var indices = [4, 13];
     indices.should.have.property('0', 4);
     indices.should.have.property('1', 13);
-    // (true).should.equal(true);
-
+    indices.should.have.property('length', 2);
   });
 
+  it('should getIndices properly 2', function(){
+    var text = "ala ma kota i ma psa";
+    var indices = RewireScope.getIndices(text, "ala");
+    indices.should.have.property('0', 0);
+    indices.should.have.property('length', 1);
+  });
+
+  it('should getIndices properly 3', function(){
+    var text = "";
+    var indices = RewireScope.getIndices(text, "ala");
+    indices.should.have.property('length', 0);
+  });
+
+  it('should getIndices with new line', function(){
+    var text = "ala ma kota\
+    i ma psa";
+    var indices = RewireScope.getIndices(text, "ma");
+    indices.should.have.property('length', 2);
+  });
+
+  it('should getIndices in function', function(){
+    var text = "(function() { return function () {\
+      getNullRule(context.isNullable, isDate.then(True).otherwise(False(\"$0 is not date\")));\
+    }; })()";
+    var indices = RewireScope.getIndices(text, "isDate");
+    indices.should.have.property('length', 1);
+  });
+
+  // it('should parse True', function() {
+  //   var fn = function(){
+  //     getNullRule(context.isNullable, isDate.then(True).otherwise(False("$0 is not date")));
+  //   };
+
+  //   var code = RewireScope.wrapFn(fn.toString());
+  //   var ctx = new esrefactor.Context(code);
+
+  //   console.log(code);
+
+  //   var id = ctx.identify(86);
+
+  //   // console.log("-------->", id);
+  //   // console.log("-------->", ctx.rename(id, "ASD"));
+  // });
+
+  it('should rewire scope', function() {
+    var fn = function(){
+      getNullRule(context.isNullable, isDate.then(True).otherwise(False("$0 is not date")));
+    };
+
+    var targetFn = function() {
+      Rules.getNullRule(context.isNullable, Rules.isDate.then(Rules.True).otherwise(Rules.False("$0 is not date")));
+    }
+
+    var key = "Rules";
+    var newScope = {
+      isDate: "",
+      True: "",
+      False: "",
+      getNullRule: ""
+    };
+
+    var fnNew = RewireScope.rewire(fn, "Rules", newScope);
+
+    var ast = esprima.parse(RewireScope.wrapFn(fnNew.toString()));
+    var targetAst = esprima.parse(RewireScope.wrapFn(targetFn.toString()));
+
+    _.isEqual(ast, targetAst).should.equal(true);
+  });
+
+  function deepEqual(obj1, obj2) {
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
+  }
 
 });
